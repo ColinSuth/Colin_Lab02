26f27bb734ac954f067eeaeff33b495ad32ae9cb
ColinSuth <colintgsutherland@gmail.com>
2023-01-30 22:08:05 -0500

    Added A lot
    Almost done lab 03

--- Lab02.py
+++ Lab02.py
@@ -3,14 +3,8 @@ def main():
     # TODO: Step 2 - Create a complex data structure
     about_me = {
         'full_name' : 'Colin Sutherland',
-        'studnet_id' : 10261370,
-        'toppings' : [
-            {
-            'TOPPINGS_1' : 'PEPPERS',
-            'TOPPINGS_2' : 'ONIONS',
-            'TOPPINGS_3' : 'SPINACH'
-            }
-        ],
+        'student_id' : 10261370,
+        'toppings' : ['PEPPERS', 'ONIONS', 'SPINACH'],
         'movie_list' : [
             {
                 'name' : 'the nice guys',
@@ -19,7 +13,7 @@ def main():
             },
             {
                 'name' : 'lord of the rings: the two towers',
-                'genre' : ' fantasy'
+                'genre' : 'fantasy'
 
             }
         ]
@@ -29,10 +23,9 @@ def main():
     # TODO: Step 3 - Add another movie to the data structure
     new_movie = {
         'name' : 'mulan',
-        'genre' : 'fantasy'
-    
+        'genre' : 'animated'
     }
-    about_me = ['movies'].append(new_movie)
+    about_me['movie_list'].append(new_movie)
     
     print_student_name_and_id(about_me)
     add_pizza_toppings(about_me, ('PEPPERONI', 'PESTO'))
@@ -42,16 +35,18 @@ def main():
     
 # TODO: Step 4 - Function that prints student name and ID	
 def print_student_name_and_id(about_me):
-    print(f'My name is {about_me["full_name"]}, but you can call me Sir {about_me["full_name".split[0]]}')
+    print(f'My name is {about_me["full_name"]}, but you can call me Sir {about_me["full_name"].split()[0]}')
     print(f'My student ID is {about_me["student_id"]}')
     return
     
 # TODO: Step 5 - Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
+    print('\nMy favourite pizza toppings are:')
+    for p in about_me['toppings']:
+        print(f'- {p}')
     about_me['toppings'].extend(toppings)
-    about_me['toppings'].sort(toppings)
-    about_me['toppings'].lower(toppings)
-
+    about_me['toppings'].sort()
+    #add lower case toppings
     return
 
 # TODO: Step 6 - Function that prints bullet list of pizza toppings
@@ -64,15 +59,18 @@ def print_pizza_toppings(about_me):
 # TODO: Step 7 - Function that prints comma-separated list of movie genres
 def print_movie_genres(about_me):
 
-    print('\nI like to watch', end='')
-    movie_genres = [g["gnere"]for g in about_me['movie_list']]
+    print('\nI like to watch ', end='')
+    movie_genres = [g["genre"]for g in about_me['movie_list']]
     movie_csl = ', '.join(movie_genres)
-    print(movie_csl)
+    print(f'{movie_csl} movies.')
     return 
 
 # TODO: Step 8 - Function that prints comma-separated list of movie titles
 def print_movie_titles(movie_list):
-
+    print('\nSome of my favourite movies are ', end='')
+    movie_title = movie_list['name']
+    movie_csl = ', '.join(movie_title)
+    print(f'{movie_csl}!')
     return
     
 if __name__ == '__main__':
